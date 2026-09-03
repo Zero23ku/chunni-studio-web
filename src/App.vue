4e6ee8423d12895c2afac01a5e9b81f2216ee9fd
Harpuia <zetsubou.harpuia@gmail.com>
2026-09-03 18:43:44 -0400

    wip: Se crean tarjetas de miembros, falta contenido

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 //import HelloWorld from './components/HelloWorld.vue'
 //import TheWelcome from './components/TheWelcome.vue'
+import Member from './components/Member.vue';
+import gikkoPngUrl from '@/assets/img/chibigikko.png';
+import gishiPngUrl from '@/assets/img/chibigishi.png';
+import nanaPngUrl from '@/assets/img/chibinana.png';
+import risaPngUrl from '@/assets/img/chibirisa.png';
+import hanachiPngUrl from '@/assets/img/chibihanachi.png';
+import pupaPngUrl from '@/assets/img/chibiupa.png';
+import harpuiaPngUrl from '@/assets/img/harpuia_chibi_2.png';
+import retniwPngUrl from '@/assets/img/retni_chibi.png';
+
+
 </script>
 
 <template>
@@ -13,10 +24,25 @@
   </header>
 
   <main>
-
+    <div class="c-wrapper">
+      <Member name="Gikko" :image="gikkoPngUrl" />
+      <Member name="Gishi" :image="gishiPngUrl" />
+      <Member name="Nana" :image="nanaPngUrl" />
+      <Member name="Risa" :image="risaPngUrl" />
+      <Member name="Hanachi" :image="hanachiPngUrl" />
+      <Member name="Pupa" :image="pupaPngUrl" />
+      <Member name="Harpuia" :image="harpuiaPngUrl" />
+      <Member name="Retniw" :image="retniwPngUrl" />
+    </div>
   </main>
 </template>
 
 <style scoped>
-
+  .c-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    margin: 0 auto;
+  }
 </style>
---
+++ src/components/Member.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+    name: String,
+    image: String
+
+});
+
+const isHovered = ref(false);
+
+function handleMouseover() {
+    isHovered.value = true;
+    console.log(isHovered.value);
+};
+
+function handleMouseleave() {
+    isHovered.value = false;
+    console.log(isHovered.value);
+
+};
+
+
+</script>
+
+<template>
+    <div class="m-wrapper" @mouseover="handleMouseover" @mouseleave="handleMouseleave">
+        <transition name="fade">
+            <h1 v-show="isHovered">{{ props.name }}</h1>
+        </transition>   
+        <img class="m-img" :src="props.image" alt="Member image" />
+    </div>
+   
+</template>
+
+<style scoped>
+.m-wrapper {
+    background-color: white;
+    height: 200px;
+    width: 360px;
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-left: 40px;
+    flex: 0 0 360px; /* fijo: no crece (0), no encoge (0) */
+}
+
+.m-wrapper:hover {
+    cursor: pointer;
+}
+
+.m-img {
+    position: absolute;
+    top: -135px;
+    right: -355px;
+    width: 700px;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+    
+</style>
