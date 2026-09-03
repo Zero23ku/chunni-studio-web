229be52f7d818d666c6787640b4ca14970cd4f71
Harpuia <zetsubou.harpuia@gmail.com>
2026-09-03 16:51:24 -0400

    wip: Iniciando sitio web, se tiene logo, color de fondo, fuente y assets

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div class="h-wrapper">
+      <img alt="Logo de Chunni Studio" class="logo" src="./assets/img/logo.png" width="40%"/>
+      <br>
+      <p>Hola soy un texto de prueba</p>
+    </div>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
